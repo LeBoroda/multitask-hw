ded0147b3c099228ce54563de3d9d1f25e358e57
Stanislav <stalevs@gmail.com>
2025-10-05 21:14:09 +0200

    add index.html for app func result  visibility

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,7 @@
 import { greetAlice } from "./myBind.js";
 import { User } from "./chainUser.js";
 import { ForceConstructor } from "./forcedUser.js";
-import {curry, sum2, sum4} from "./curry.js";
+import { curry, sum2, sum4 } from "./curry.js";
 
 console.log(greetAlice("!"));
 
@@ -16,8 +16,8 @@ console.log(dU.name);
 const curried2 = curry(sum2); // 3
 const curried4 = curry(sum4); // 14
 
-console.log(sum2(1, 2));
-console.log(curried2(1)(2));
+console.log(`Sum2 result: ${sum2(1, 2)}`);
+console.log(`Curry result: ${curried2(1)(2)}`);
 
-console.log(sum4(2, 3, 4, 5));
-console.log(curried4(2)(3)(4)(5));
+console.log(`Sum4 result: ${sum4(2, 3, 4, 5)}`);
+console.log(`Curry result: ${curried4(2)(3)(4)(5)}`);
