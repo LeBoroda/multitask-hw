76c3283df4a0d72e70dec4c59a05f936914b2f49
Stanislav <stalevs@gmail.com>
2025-10-05 20:25:33 +0200

    add force constructor and tests

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,13 @@
 import { greetAlice } from "./myBind.js";
-import {User} from "./chainUser.js";
+import { User } from "./chainUser.js";
+import {ForceConstructor} from "./forcedUser.js";
 
 console.log(greetAlice("!"));
 
 const u = new User();
 u.askName().askAge().showAgeInConsole().showNameInAlert();
+
+const fU = ForceConstructor('ForcedUser');
+console.log(fU.name);
+const dU = new ForceConstructor('DeliberateUser');
+console.log(dU.name);
